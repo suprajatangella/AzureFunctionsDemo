5e9aa06cc14d82c905009f5ed1be564af7bd0777
Supraja Tangella <supraja.tangella@gmail.com>
2025-09-15 13:50:47 +0100

    Added TimerTigger.cs file for executing Scheduled function in a regular interval

---
+++ AzureFunctionsDemo/TimerTrigger.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionsDemo;
+
+public class TimerTrigger
+{
+    private readonly ILogger _logger;
+
+    public TimerTrigger(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<TimerTrigger>();
+    }
+
+    [Function("CallScheduledTaskEveryMinute")]
+    public void CallScheduledTaskEveryMinute([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
+    {
+        _logger.LogInformation("C# Timer trigger function executed at: {executionTime}", DateTime.Now);
+        
+        if (myTimer.ScheduleStatus is not null)
+        {
+            _logger.LogInformation("Next timer schedule at: {nextSchedule}", myTimer.ScheduleStatus.Next);
+        }
+    }
+}
